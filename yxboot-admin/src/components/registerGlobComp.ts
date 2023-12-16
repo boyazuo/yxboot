4b37da481576d2358fea47cb8bc5126d2662ca0e
zuopeibo <6989006@qq.com>
2023-12-16 19:30:02 +0800

    feat: 增加按钮组件展示页面

--- yxboot-admin/src/components/registerGlobComp.ts
+++ yxboot-admin/src/components/registerGlobComp.ts
@@ -1,7 +1,6 @@
 import Antd from 'ant-design-vue'
 import type { App } from 'vue'
-import { Button } from './Button'
 
 export function registerGlobComp(app: App) {
-  app.use(Antd).use(Button)
+  app.use(Antd)
 }
