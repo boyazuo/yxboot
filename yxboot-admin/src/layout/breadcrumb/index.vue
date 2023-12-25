b52134a07c6d64c86ee51d3485357ff5c2542072
liangliang <liangliang8768@qq.com>
2023-12-25 22:59:22 +0800

    fix:修复设置中，Logo显示功能

---
+++ yxboot-admin/src/layout/breadcrumb/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-breadcrumb :routes="routes">
+    <template #itemRender="{ route }">
+      <span>
+        {{ route.meta.title }}
+      </span>
+    </template>
+  </a-breadcrumb>
+</template>
+<script lang="ts" setup>
+  import { useRouter } from 'vue-router'
+  const routes = ref<RouteLocationMatched[]>([])
+  const { currentRoute } = useRouter()
+
+  watchEffect(() => {
+    routes.value = [currentRoute.value]
+  })
+</script>
+<style lang="less"></style>
