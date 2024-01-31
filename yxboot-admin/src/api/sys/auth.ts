f9fb242b7374f9541520169dcdb55f3a61debb93
zuopeibo <6989006@qq.com>
2024-01-31 11:29:27 +0800

    refactor: 重写登录页面

--- yxboot-admin/src/api/sys/auth.ts
+++ yxboot-admin/src/api/sys/auth.ts
@@ -5,9 +5,11 @@ import { LoginParams } from '../model/userModel'
 enum Api {
   login = '/auth/login',
   profile = '/auth/profile',
-  permissions = '/auth/permissions'
+  permissions = '/auth/permissions',
+  captcha = '/auth/captcha'
 }
 
 export const login = (params: LoginParams) => http.post<BasicResultModel>({ url: Api.login, params })
 export const profile = () => http.get<BasicResultModel>({ url: Api.profile })
 export const permissions = () => http.get<BasicResultModel>({ url: Api.permissions })
+export const captcha = () => http.get<BasicResultModel>({ url: Api.captcha })
--- yxboot-admin/src/views/login.vue
+++ yxboot-admin/src/views/login.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="loginpage">
-    <div class="title-box">
-      <img :class="{ 'title-img': true, small: !titleOption.subtitle }" src="/logo.png" />
-      <div class="title">
-        <div class="headline">{{ titleOption.headline }}</div>
-        <div class="subtitle" v-if="titleOption.subtitle">{{ titleOption.subtitle }}</div>
+  <div class="page">
+    <div class="top">
+      <div class="header">
+        <img :class="{ logo: true }" src="/logo.png" />
+        <div class="title">{{ titleOption.title }}</div>
       </div>
+      <div class="desc" v-if="titleOption.desc">{{ titleOption.desc }}</div>
     </div>
     <a-form ref="formRef" class="form" :model="formData" :rules="rules" size="normal" @keypress.enter="handleLogin">
       <a-form-item name="username">
         <a-input
           v-model:value="formData.username"
-          placeholder="请输入用户名"
-          :prefix="h(Icon, { icon: 'ph:user-bold' })"
+          placeholder="用户名: admin"
+          :prefix="h(Icon, { icon: 'ant-design:user-outlined' })"
           size="large"
           :maxlength="20"
           autocomplete="username"
@@ -21,7 +21,7 @@
       <a-form-item name="password">
         <a-input-password
           v-model:value="formData.password"
-          placeholder="请输入密码"
+          placeholder="密码: 123456"
           :prefix="h(Icon, { icon: 'carbon:password' })"
           size="large"
           :maxlength="20"
@@ -29,9 +29,8 @@
         />
       </a-form-item>
       <a-form-item name="captchaCode">
-        <div class="captchaCode-row">
+        <div class="flex justify-between">
           <a-input
-            class="captchaCode-input"
             v-model:value="formData.captchaCode"
             placeholder="请输入验证码"
             :prefix="h(Icon, { icon: 'material-symbols:verified-user-outline-rounded' })"
@@ -39,35 +38,61 @@
             allowClear
             :maxlength="4"
           />
-          <img class="captchaCode-img" :src="captcha + thisTime" @click="handlechangeCaptcha" />
+          <img class="captcha" :src="formData.captchaImage" @click="handleChangeCaptcha" />
         </div>
       </a-form-item>
+      <a-row>
+        <a-col :span="12">
+          <a-form-item>
+            <!-- No logic, you need to deal with it yourself -->
+            <a-checkbox v-model:checked="rememberMe" size="small"> 自动登录 </a-checkbox>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item :style="{ 'text-align': 'right' }">
+            <!-- No logic, you need to deal with it yourself -->
+            <a-button type="link"> 忘记密码? </a-button>
+          </a-form-item>
+        </a-col>
+      </a-row>
       <a-form-item>
-        <a-button class="loginBtn" type="primary" block :loading="loading" @click="handleLogin">登录</a-button>
+        <a-button type="primary" size="large" block :loading="loading" @click="handleLogin">登录</a-button>
       </a-form-item>
+      <div class="login-other-way">
+        <!-- No logic, you need to deal with it yourself -->
+        <span>其他登录方式</span>
+        <GithubFilled />
+        <WechatFilled />
+        <AlipayCircleFilled />
+        <!-- No logic, you need to deal with it yourself -->
+        <a-button type="link" style="float: right"> 注册账号 </a-button>
+      </div>
     </a-form>
   </div>
 </template>
 <script lang="ts" setup>
+  import { captcha } from '@/api/sys/auth'
   import Icon from '@/components/Icon/index'
   import { useGlobSetting } from '@/hooks/setting'
   import { useUserStore } from '@/store/modules/user'
+  import { AlipayCircleFilled, GithubFilled, WechatFilled } from '@ant-design/icons-vue'
   import { message } from 'ant-design-vue'
 
   const titleOption = {
-    headline: '' || useGlobSetting().title,
-    subtitle: '前后端分离快速开发平台'
+    title: 'YXBoot' || useGlobSetting().title,
+    desc: 'YXBoot 一个开箱即用的前后端解决方案'
   }
 
   const formData = reactive({
-    username: 'admin',
-    password: '123456',
-    captchaCode: ''
+    username: '',
+    password: '',
+    captchaCode: '',
+    captchaImage: ''
   })
   const rules = {
-    username: [{ required: true, message: '用户名不能为空', trigger: 'blur' }],
-    password: [{ required: true, message: '密码不能为空', trigger: 'blur' }],
-    captchaCode: [{ required: true, message: '验证码不能为空', trigger: 'blur' }]
+    username: [{ required: true, message: '请输入用户名' }],
+    password: [{ required: true, message: '请输入密码' }],
+    captchaCode: [{ required: true, message: '请输入验证码' }]
   }
 
   const formRef = ref()
@@ -75,6 +100,8 @@
   const route = useRoute()
   const userStore = useUserStore()
   const loading = ref(false)
+  const rememberMe = ref(false)
+
   const handleLogin = async () => {
     formRef.value.validate().then(() => {
       loading.value = true
@@ -88,11 +115,11 @@
             else router.replace(toPath)
           } else {
             message.error(res.msg)
-            handlechangeCaptcha()
+            handleChangeCaptcha()
           }
         })
         .catch(() => {
-          handlechangeCaptcha()
+          handleChangeCaptcha()
         })
         .finally(() => {
           loading.value = false
@@ -100,52 +127,52 @@
     })
   }
 
-  const captcha = '/api/auth/captcha'
-  let thisTime: any = ref('')
-  const handlechangeCaptcha = () => {
-    thisTime.value = `?v=${Date.now()}`
+  const handleChangeCaptcha = () => {
     formData.captchaCode = ''
+    captcha().then((res) => {
+      formData.captchaImage = res.data
+    })
   }
+
+  onMounted(() => {
+    handleChangeCaptcha()
+  })
 </script>
 <style lang="less" scoped>
-  .loginpage {
+  .page {
     display: flex;
-    flex-flow: column nowrap;
-    justify-content: flex-start;
+    flex-direction: column;
     align-items: center;
     width: 100%;
     height: 100vh;
-    padding: 200px 0;
+    padding: 120px 0;
     background-image: url('@/assets/images/login/login_bg.svg');
 
-    .title-box {
-      display: flex;
-      align-items: center;
-
-      .title-img {
-        display: inline-block;
-        width: 70px;
-        height: 70px;
-        margin-right: 10px;
-
-        &.small {
-          width: 40px;
-          height: 40px;
-        }
-      }
-
-      .title {
+    .top {
+      .header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         user-select: none;
 
-        .headline {
+        .logo {
+          width: 48px;
+          height: 48px;
+          margin-right: 15px;
+        }
+
+        .title {
           font-size: 30px;
           font-weight: bold;
         }
+      }
 
-        .subtitle {
-          float: right;
-          color: #808695;
-        }
+      .desc {
+        text-align: center;
+        margin-top: 12px;
+        margin-bottom: 30px;
+        font-size: 14px;
+        color: #808695;
       }
     }
 
@@ -153,20 +180,27 @@
       margin-top: 30px;
       width: 400px;
 
-      .captchaCode-row {
+      .captcha {
         height: 40px;
-        display: flex;
-        justify-content: space-between;
-
-        .captchaCode-img {
-          margin-left: 8px;
-          border: 1px solid #d9d9d9;
-        }
+        margin-left: 8px;
+        border: 1px solid #d9d9d9;
       }
+    }
 
-      .loginBtn {
-        height: 40px;
-        justify-content: center;
+    .login-other-way {
+      height: 32px;
+      line-height: 32px;
+      margin-bottom: 40px;
+
+      .anticon {
+        margin-left: 15px;
+        color: #888;
+        font-size: 22px;
+        cursor: pointer;
+
+        &:hover {
+          color: var(--primary-color);
+        }
       }
     }
   }
