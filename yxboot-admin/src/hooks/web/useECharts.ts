cbe38debab9a440df1d4233386513050ee3953aa
zuopeibo <6989006@qq.com>
2024-02-04 20:00:56 +0800

    feat: 增加分析页

---
+++ yxboot-admin/src/hooks/web/useECharts.ts
@@ -0,0 +1,127 @@
+import type { EChartsOption } from 'echarts'
+import type { Ref } from 'vue'
+
+import { useBreakpoint } from '@/hooks/event/useBreakpoint'
+import { useEventListener } from '@/hooks/event/useEventListener'
+import { useSiderSetting, useThemeSetting } from '@/hooks/setting'
+import echarts from '@/utils/lib/echarts'
+import { tryOnUnmounted, useDebounceFn, useTimeoutFn } from '@vueuse/core'
+import { computed, nextTick, ref, unref, watch } from 'vue'
+
+export function useECharts(elRef: Ref<HTMLDivElement>, theme: 'light' | 'dark' | 'default' = 'default') {
+  const { getTheme: getSysDarkMode } = useThemeSetting()
+  const { getCollapsed } = useSiderSetting()
+
+  const getDarkMode = computed(() => {
+    return theme === 'default' ? getSysDarkMode.value : theme
+  })
+  let chartInstance: echarts.ECharts | null = null
+  let resizeFn: Fn = resize
+  const cacheOptions = ref({}) as Ref<EChartsOption>
+  let removeResizeFn: Fn = () => {}
+
+  resizeFn = useDebounceFn(resize, 200)
+
+  const getOptions = computed(() => {
+    if (getDarkMode.value !== 'dark') {
+      return cacheOptions.value as EChartsOption
+    }
+    return {
+      backgroundColor: 'transparent',
+      ...cacheOptions.value
+    } as EChartsOption
+  })
+
+  function initCharts(t = theme) {
+    const el = unref(elRef)
+    if (!el || !unref(el)) {
+      return
+    }
+
+    chartInstance = echarts.init(el, t)
+    const { removeEvent } = useEventListener({
+      el: window,
+      name: 'resize',
+      listener: resizeFn
+    })
+    removeResizeFn = removeEvent
+    const { widthRef, screenEnum } = useBreakpoint()
+    if (unref(widthRef) <= screenEnum.MD || el.offsetHeight === 0) {
+      useTimeoutFn(() => {
+        resizeFn()
+      }, 30)
+    }
+  }
+
+  function setOptions(options: EChartsOption, clear = true) {
+    cacheOptions.value = options
+    return new Promise((resolve) => {
+      if (unref(elRef)?.offsetHeight === 0) {
+        useTimeoutFn(() => {
+          setOptions(unref(getOptions))
+          resolve(null)
+        }, 30)
+      }
+      nextTick(() => {
+        useTimeoutFn(() => {
+          if (!chartInstance) {
+            initCharts(getDarkMode.value as 'default')
+
+            if (!chartInstance) return
+          }
+          clear && chartInstance?.clear()
+
+          chartInstance?.setOption(unref(getOptions))
+          resolve(null)
+        }, 30)
+      })
+    })
+  }
+
+  function resize() {
+    chartInstance?.resize({
+      animation: {
+        duration: 300,
+        easing: 'quadraticIn'
+      }
+    })
+  }
+
+  watch(
+    () => getDarkMode.value,
+    (theme) => {
+      if (chartInstance) {
+        chartInstance.dispose()
+        initCharts(theme as 'default')
+        setOptions(cacheOptions.value)
+      }
+    }
+  )
+
+  watch(getCollapsed, (_) => {
+    useTimeoutFn(() => {
+      resizeFn()
+    }, 300)
+  })
+
+  tryOnUnmounted(() => {
+    if (!chartInstance) return
+    removeResizeFn()
+    chartInstance.dispose()
+    chartInstance = null
+  })
+
+  function getInstance(): echarts.ECharts | null {
+    if (!chartInstance) {
+      initCharts(getDarkMode.value as 'default')
+    }
+    return chartInstance
+  }
+
+  return {
+    setOptions,
+    resize,
+    echarts,
+    getInstance
+  }
+}
---
+++ yxboot-admin/src/views/dashboard/analysis/components/SiteAnalysis.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-card :tab-list="tabListTitle" v-bind="$attrs" :active-tab-key="activeKey" @tab-change="onTabChange">
+    <p v-if="activeKey === 'tab1'">
+      <VisitAnalysis />
+    </p>
+    <p v-if="activeKey === 'tab2'">
+      <VisitAnalysisBar />
+    </p>
+  </a-card>
+</template>
+<script lang="ts" setup>
+  import VisitAnalysis from './VisitAnalysis.vue'
+  import VisitAnalysisBar from './VisitAnalysisBar.vue'
+
+  const activeKey = ref('tab1')
+
+  const tabListTitle = [
+    {
+      key: 'tab1',
+      tab: '流量趋势'
+    },
+    {
+      key: 'tab2',
+      tab: '访问量'
+    }
+  ]
+
+  function onTabChange(key) {
+    activeKey.value = key
+  }
+</script>
